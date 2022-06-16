92253c5c6fbfdcf9a3c4643e3da9689ed76b7fc9
rahul <rahulsrmacs@gmail.com>
2022-06-17 00:54:23 +0530

    created category component

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import axios from 'axios';
 
-createApp(App).mount('#app')
+window.axios = axios
+import swal from 'sweetalert';
+window.Swal = swal;
+
+createApp(App).use(router).mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+<h1>App Home</h1>
+</template>
+
+<script>
+</script>
